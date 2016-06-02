f14a6126af9bc0d93e4eeed3425ca240ef44e773
ShelinShelin <1332109343@qq.com>
2016-06-02 10:53:12 +0800

    no message

--- NetworkDemo/XLNetworkPrivate.m
+++ NetworkDemo/XLNetworkPrivate.m
@@ -83,5 +83,36 @@
     }
 }
 
+@end
+
+
+
+
+@implementation XLChainRequest (RequestAccessory)
+
+- (void)accessoriesWillStartCallBack {
+    for (id<XLRequestAccessory> accessory in self.requestAccessories) {
+        if ([accessory respondsToSelector:@selector(requestWillStart:)]) {
+            [accessory requestWillStart:self];
+        }
+    }
+}
+
+- (void)accessoriesWillStopCallBack {
+    for (id<XLRequestAccessory> accessory in self.requestAccessories) {
+        if ([accessory respondsToSelector:@selector(requestWillStop:)]) {
+            [accessory requestWillStop:self];
+        }
+    }
+}
+
+- (void)accessoriesDidStopCallBack {
+    for (id<XLRequestAccessory> accessory in self.requestAccessories) {
+        if ([accessory respondsToSelector:@selector(requestDidStop:)]) {
+            [accessory requestDidStop:self];
+        }
+    }
+}
+
 
 @end
